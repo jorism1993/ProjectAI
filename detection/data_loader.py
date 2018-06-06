613b038b322ae735f4fb62a7e2a71c4a95172abe
jim3101 <jimhoekstra2@gmail.com>
2018-06-06 15:21:02 +0200

    added script that rewrites the annotations csv to include each image only once with all the bbox information, and adapted the data_loader to handle this

--- detection/data_loader.py
+++ detection/data_loader.py
@@ -7,11 +7,12 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 import pandas as pd
+import csv
 
 
 
 class DetectionDataset(Dataset):
-	def __init__(self, csv_file, root_dir, transform=None):
+	def __init__(self, csv_path, root_dir, transform=None, use_superclass=False):
 		"""
 		Args:
 		    csv_file (string): Path to the csv file with annotations.
@@ -19,51 +20,82 @@ class DetectionDataset(Dataset):
 		    transform (callable, optional): Optional transform to be applied
 		        on a sample.
 		"""
-		self.annotations = pd.read_csv(csv_file, sep=';')
+		self.annotations = []
+		with open(csv_path, newline='') as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=';', quotechar='|')
+			for row in csv_reader:
+				self.annotations.append(row)
+
 		self.root_dir = root_dir
 		self.transform = transform
+		self.use_superclass = use_superclass
+
 
 	def __len__(self):
 		return len(self.annotations)
 
+
 	def __getitem__(self, idx):
-		img_name = os.path.join(self.root_dir, self.annotations.iloc[idx, 0])
+		img_name = os.path.join(self.root_dir, self.annotations[idx][0])
+
+		n_bboxes = len(self.annotations[idx])
+		n_bboxes -= 1 # Don't count the name
+		n_bboxes /= 6 # There are 6 information fields per bounding box
+		n_bboxes = int(n_bboxes)
 
 		# Change the extension from jp2 to jpg
 		img_name = img_name[:-3] + 'jpg'
 
 		image = io.imread(img_name)
+		image = np.array(image)
+		image = np.moveaxis(image, -1, 0)
+		image = np.expand_dims(image, axis=0)
 
-		bbox = self.annotations.iloc[idx, 1:5].as_matrix()
-		bbox = bbox.astype('float').reshape(-1, 4)
+		image_info = np.array([image.shape[2], image.shape[3], 1.5])
 
-		sample = (image, bbox)
+		bboxes = np.zeros((1, 20, 5))
 
-		if self.transform:
-		    sample = self.transform(sample)
+		for i in range(n_bboxes):
+			bbox = self.annotations[idx][i*6+1: i*6+5]
+			class_label = self.annotations[idx][i*6+5]
+			superclass_label = self.annotations[idx][i*6+6]
 
-		return sample
+			if self.use_superclass:
+				bbox.extend(int(float(superclass_label)))
+			else:
+				bbox.append(int(float(class_label)))
+
+			bboxes[0, i, :] = bbox
+
+		data = (image, image_info, bboxes, n_bboxes)
+
+		return data
+
 
 	def show_example(index=0):
 		fig = plt.figure()
 		ax = plt.subplot(1, 1, 1)
 
-		sample = detection_dataset[6]
+		sample = detection_dataset[5]
 
 		plt.tight_layout()
 		im = sample[0]
-		imsize = im.shape[0:2]
-		bbox = sample[1]
-		bbox = bbox.reshape(-1)
+		im = np.squeeze(im, axis=0)
 
-		rect = patches.Rectangle((bbox[0],bbox[1]), bbox[2]-bbox[0], bbox[3]-bbox[1],linewidth=1,edgecolor='r',facecolor='none')
-		ax.add_patch(rect)
+		bboxes = sample[2][0]
+		n_bboxes = sample[3]
+		for i in range(n_bboxes):	
+			bbox = bboxes[i]
+			bbox = bbox.reshape(-1)
 
+			rect = patches.Rectangle((bbox[0],bbox[1]), bbox[2]-bbox[0], bbox[3]-bbox[1],linewidth=1,edgecolor='r',facecolor='none')
+			ax.add_patch(rect)
 
+		im = np.moveaxis(im, 0, -1)	
 		ax.imshow(im)
 		plt.show()
 
 
 
-detection_dataset = DetectionDataset(csv_file='../data/detection_data/annotations_test.txt', root_dir='../detection_data/')
+detection_dataset = DetectionDataset(csv_path='../data/detection_data/annotations_multiple_boxes.txt', root_dir='../data/detection_data/')
 detection_dataset.show_example()
